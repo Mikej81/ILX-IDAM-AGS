d2926c2669d1bad90f2e4dd6a34c5f660a4bd68d
Mikej81 <stumpj@gmail.com>
2016-04-18 10:46:32 -0700

    base_nonfunctional

--- extensions/ldap_extension/index.js
+++ extensions/ldap_extension/index.js
@@ -1,23 +1,73 @@
 var f5 = require('f5-nodejs');
 var ldap = require('ldapjs');
+var ldap_bind_url = 'ldaps://192.168.2.25:636';
+var ldap_bind_dn = '';
+var ldap_bind_pwd = '';
+
 
 /* Create a new rpc server for listening to TCL iRule calls. */
 var ilx = new f5.ILXServer();
 
 ilx.addMethod('ldap_modify', function(req,res) {
     var client = ldap.createClient({
-        url: 'ldaps://192.168.2.25:636'
+        url: ldap_bind_url
         
     });
-    
+    if (ldap_bind_url.startsWith('ldaps')) {
+    	var opts = {
+    		//how to read in CA PEM
+  			ca: [fs.readFileSync('mycacert.pem')]
+		};
+
+		client.starttls(opts, function(err, res) {
+  			assert.ifError(err);
+  			// Client communication now TLS protected
+		});
+
+    } else {
+    	//Create and Modify will fail over unenc-ldap
+    }
+    client.bind(ldap_bind_dn, ldap_bind_pwd, function(err) {
+  		assert.ifError(err);
+	});
+	client.modify('cn=foo, o=example', change, function(err) {
+  		assert.ifError(err);
+	});
+	client.unbind(function(err) {
+  		assert.ifError(err);
+	});
     
 });
 ilx.addMethod('ldap_create', function(req,res) {
     var client = ldap.createClient({
-        url: 'ldaps://192.168.2.25:636'
+        url: ldap_bind_url
         
     });
-    
+
+        if (ldap_bind_url.startsWith('ldaps')) {
+    	var opts = {
+    		//how to read in CA PEM
+  			ca: [fs.readFileSync('mycacert.pem')]
+		};
+
+		client.starttls(opts, function(err, res) {
+  			assert.ifError(err);
+  			// Client communication now TLS protected
+		});
+
+    } else {
+    	//Create and Modify will fail over unenc-ldap
+    }
+
+    client.bind(ldap_bind_dn, ldap_bind_pwd, function(err) {
+  		assert.ifError(err);
+	});
+	client.add('cn=foo, o=example', entry, function(err) {
+  		assert.ifError(err);
+	});
+	client.unbind(function(err) {
+  		assert.ifError(err);
+	});    
     
 });
 
--- rules/ldap_irulelx.tcl
+++ rules/ldap_irulelx.tcl
@@ -21,57 +21,82 @@ set ldap_user_dn_suffix 'OU=Lab Users,DC=f5lab,DC=com'
 
      switch [ACCESS::policy agent_id] {
         "ldap_create" {
-          set ldap_response [ILX::call $ldap_handle ldap_create $ldap_ldif_data ]}
+        	#do some LDIF generation magic
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: add\r\n"
+        	append ldap_ldif_data "cn: [table lookup -notouch -subtable $table_name cn]\r\n"
+        	append ldap_ldif_data "sAMAccountName: $table_name\r\n"
+        	append ldap_ldif_data "userPrincipalName: [table lookup -notouch -subtable $table_name upn]\r\n"
+        	append ldap_ldif_data "\r\n"
+        	#Required for set password
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: modify\r\n"
+        	append ldap_ldif_data "replace: unicodePwd\r\n"
+        	#Sets password to pass@word1
+        	append ldap_ldif_data "unicodePwd:: IgBBAG4ARQB4AGEAbQBwAGwAZQBQAGEAcwBzAHcAbwByAGQAMQAhACIA\r\n"
+        	append ldap_ldif_data "\r\n"
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: modify\r\n"
+        	#Required for creating new user
+        	append ldap_ldif_data "replace: userAccountControl\r\n"
+        	append ldap_ldif_data "userAccountControl: 512\r\n"
+        	#EOF
+        	append ldap_ldif_data "-"
+
+        	set ldap_response [ILX::call $ldap_handle ldap_create $ldap_ldif_data ]}
         "ldap_modify" {
-          set ldap_response [ILX::call $ldap_handle ldap_modify $ldap_ldif_data ]}
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: modify\r\n"
+
+        	set ldap_response [ILX::call $ldap_handle ldap_modify $ldap_ldif_data ]}
         "ldif_create" {
-        append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
-        append ldap_ldif_data "changetype: add\r\n"
-        append ldap_ldif_data "objectClass: user\r\n"   
-        append ldap_ldif_data "cn: [table lookup -notouch -subtable $table_name cn]\r\n"
-        append ldap_ldif_data "sAMAccountName: $table_name\r\n"
-        append ldap_ldif_data "userPrincipalName: [table lookup -notouch -subtable $table_name upn]\r\n"
-        append ldap_ldif_data "\r\n"
-        #Required for set password
-        append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
-        append ldap_ldif_data "changetype: modify\r\n"
-        append ldap_ldif_data "replace: unicodePwd\r\n"
-        append ldap_ldif_data "unicodePwd:: IgBBAG4ARQB4AGEAbQBwAGwAZQBQAGEAcwBzAHcAbwByAGQAMQAhACIA\r\n"
-        append ldap_ldif_data "\r\n"
-        append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
-        append ldap_ldif_data "changetype: modify\r\n"
-        #Required for creating new user
-        append ldap_ldif_data "replace: userAccountControl\r\n"
-        append ldap_ldif_data "userAccountControl: 512\r\n"
-        #EOF
-        append ldap_ldif_data "-"
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: add\r\n"
+        	append ldap_ldif_data "objectClass: user\r\n"   
+        	append ldap_ldif_data "cn: [table lookup -notouch -subtable $table_name cn]\r\n"
+        	append ldap_ldif_data "sAMAccountName: $table_name\r\n"
+        	append ldap_ldif_data "userPrincipalName: [table lookup -notouch -subtable $table_name upn]\r\n"
+        	append ldap_ldif_data "\r\n"
+        	#Required for set password
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: modify\r\n"
+        	append ldap_ldif_data "replace: unicodePwd\r\n"
+        	append ldap_ldif_data "unicodePwd:: IgBBAG4ARQB4AGEAbQBwAGwAZQBQAGEAcwBzAHcAbwByAGQAMQAhACIA\r\n"
+        	append ldap_ldif_data "\r\n"
+        	append ldap_ldif_data "dn: [table lookup -notouch -subtable $table_name dn]\r\n"
+        	append ldap_ldif_data "changetype: modify\r\n"
+        	#Required for creating new user
+        	append ldap_ldif_data "replace: userAccountControl\r\n"
+        	append ldap_ldif_data "userAccountControl: 512\r\n"
+        	#EOF
+        	append ldap_ldif_data "-"
         }
         "CERTPROC" {
-                if { [ACCESS::session data get session.ssl.cert.x509extension] contains "othername:UPN<" } {
-         set tmpupn [findstr [ACCESS::session data get session.ssl.cert.x509extension] "othername:UPN<" 14 ">"]
-         ACCESS::session data set session.custom.certupn $tmpupn
-         log local0. "Extracted EDIPI: $tmpupn"
-        }
-        if { [ACCESS::session data get session.ssl.cert.x509extension] contains "email:" } {
-          set tmpemail [findstr [ACCESS::session data get session.ssl.cert.x509extension] "email:" 6 ","]
-          ACCESS::session data set session.custom.email $tmpemail
-          log local0. "Extracted Email Field: $tmpemail"
-        }
-        if { [ACCESS::session data get session.ssl.cert.subject] contains "CN="} {
-         set tmpcn [findstr [ACCESS::session data get session.ssl.cert.subject] "CN=" 3 ,]
-         ACCESS::session data set session.custom.tmpcn $tmpcn
-         log local0. "Extracted CN: $tmpcn"
-        }
-        if { [ACCESS::session data get session.ssl.cert.subject] contains "C="} {
-         set tmpc [findstr [ACCESS::session data get session.ssl.cert.subject] "C=" 2 ,]
-         ACCESS::session data set session.custom.country $tmpc
-         log local0. "Extracted Country: $tmpc"
-        }
-        if { [ACCESS::session data get session.ssl.cert.subject] contains "O="} {
-         set tmpo [findstr [ACCESS::session data get session.ssl.cert.subject] "O=" 2 ,]
-         ACCESS::session data set session.custom.org $tmpo
-         log local0. "Extracted Org: $tmpo"
-        }
+        	if { [ACCESS::session data get session.ssl.cert.x509extension] contains "othername:UPN<" } {
+        		set tmpupn [findstr [ACCESS::session data get session.ssl.cert.x509extension] "othername:UPN<" 14 ">"]
+        		ACCESS::session data set session.custom.certupn $tmpupn
+        		log local0. "Extracted EDIPI: $tmpupn"
+	        }
+	        if { [ACCESS::session data get session.ssl.cert.x509extension] contains "email:" } {
+	          set tmpemail [findstr [ACCESS::session data get session.ssl.cert.x509extension] "email:" 6 ","]
+	          ACCESS::session data set session.custom.email $tmpemail
+	          log local0. "Extracted Email Field: $tmpemail"
+	        }
+	        if { [ACCESS::session data get session.ssl.cert.subject] contains "CN="} {
+	         set tmpcn [findstr [ACCESS::session data get session.ssl.cert.subject] "CN=" 3 ,]
+	         ACCESS::session data set session.custom.tmpcn $tmpcn
+	         log local0. "Extracted CN: $tmpcn"
+	        }
+	        if { [ACCESS::session data get session.ssl.cert.subject] contains "C="} {
+	         set tmpc [findstr [ACCESS::session data get session.ssl.cert.subject] "C=" 2 ,]
+	         ACCESS::session data set session.custom.country $tmpc
+	         log local0. "Extracted Country: $tmpc"
+	        }
+	        if { [ACCESS::session data get session.ssl.cert.subject] contains "O="} {
+	         set tmpo [findstr [ACCESS::session data get session.ssl.cert.subject] "O=" 2 ,]
+	         ACCESS::session data set session.custom.org $tmpo
+	         log local0. "Extracted Org: $tmpo"
+	        }
         #Figure out how to pull validity / expiration dates
         if { [ACCESS::session data get session.ssl.cert.end] ne ""} {
          set expire [ACCESS::session data get session.ssl.cert.end]
